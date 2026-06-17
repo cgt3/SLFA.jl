9e1857efa606d8e3472c975d4cda7df9ea2e6a2e
BradenKing <bradenking@boisestate.edu>
2026-06-17 16:01:04 -0600

    Test: Inner constructors complete
    And scalar/scalar 1D outer constructor

--- test/RBF_test.jl
+++ test/RBF_test.jl
@@ -1,90 +1,95 @@
 using SLFA
 
 #Tests 1-5
-@testset  "Evaluating a 1D Gaussian RBF" begin
+@testset  "Gaussian{Isotropic, T_x, 1}: 1D Gaussian RBF Inner Contstructor" begin
     x0 = 1.0;
     w = 2.0;
 
-    rbf_iso = Gaussian(x0, w)
-    @test rbf_iso isa Gaussian{Isotropic, typeof(x0), 1}
-    @test rbf_iso(x0) == 1.0
-    @test rbf_iso(2) == 0.01831563888873418 #Add a rounding handler
-    
-    # Now we test it with parametric constructor
-    rbf_iso_param = Gaussian{Isotropic,typeof(x0),1}(x0,w)
-    @test rbf_iso isa Gaussian{Isotropic, typeof(x0), 1} 
-
-    # Test with an array of parameters
-    theta = [x0;w]
-    rbf_iso_paramarray = Gaussian{Isotropic,typeof(x0),length(x0)}(theta)
-    @test rbf_iso_paramarray isa Gaussian{Isotropic, typeof(x0), 1} 
-    # eval_rbf_iso_paramarray_x0 = SLFA.eval_phi(x0,theta,rbf_iso_paramarray)
-    # eval_rbf_iso_paramarray_2 = SLFA.eval_phi(2,theta,Gaussian{Isotropic,typeof(theta[1]),length(x0)})
-    # @test eval_rbf_iso_paramarray_x0 == 1.0
-    # @test eval_rbf_iso_paramarray_x == 0.01831563888873418
+    rbf = Gaussian{Isotropic,typeof(x0),1}(x0,w)
+    @test rbf.x0 == 1.0
+    @test rbf.w == 2.0
 end
 
-#Tests 6-9
-@testset  "Evaluating a Vector/Scalar 1D Gaussian RBF" begin
+@testset "Gaussian{Isotropic, T_x, dim}: ND/1D Gaussian RBF Inner Constructor" begin
     x0 = [1.0];
     w = 2.0;
+    try
+        rbf = Gaussian{Isotropic, typeof(x0[1]), 1.5}(x0, w)
+        @test false #Fails to catch the non-integer dim
+    catch e
+        @test e == "SLFA.Gaussian: dim must be an integer."
+    end
     
-    rbf_iso = Gaussian(x0, w)
-    @test rbf_iso isa Gaussian{Isotropic, typeof(x0[1]), 1}
-    @test rbf_iso(x0[1]) == 1.0 
-    @test rbf_iso(2) == 0.01831563888873418 #Add a rounding handler here
+    try
+        rbf = Gaussian{Isotropic, typeof(x0[1]), 2}(x0, w)
+        @test false #Fails to catch the non-matching dim
+    catch e
+        @test e == "SLFA.Gaussian: dim does not match length(x0)."
+    end
     
-    # Now we test it with parametric constructor
-    rbf_iso_param = Gaussian{Isotropic,typeof(x0[1]),1}(x0,w)
-    @test rbf_iso_param isa Gaussian{Isotropic, typeof(x0[1]), 1} 
+    try
+        x_empty = Float64[]
+        rbf = Gaussian{Isotropic, typeof(x0[1]), 0}(x_empty, w)
+        @test false #Fails to catch the empty x0
+    catch e
+        @test e == "SLFA.Gaussian: x0 = []"
+    end
+    
+    rbf = Gaussian{Isotropic, typeof(x0[1]), length(x0)}(x0, w)
+    @test rbf.x0 == 1.0
+    @test rbf.w == 2.0
+
+    rbf = Gaussian{Isotropic, typeof(x0[1]), 2}([x0[1], x0[1]], w)
+    @test rbf.x0 == [1.0, 1.0]
+    @test rbf.w == 2.0
 end
 
-#Tests 10-14
-@testset  "Evaluating a ND Isotropic Gaussian RBF" begin
-    x0 = [1.0, 2.0, 3.0, 4.0, 5.0];
-    w = 1.5;
+@testset "Gaussian{Anisotropic{Aligned}, T_x, dim}: Anisotropic RBF Inner Constructor" begin
+    x0 = [1.0, 1.0];
+    w = [2.0, 2.0];
+    try
+        rbf = Gaussian{Anisotropic{Aligned}, typeof(x0[1]), 1.5}(x0, w)
+        @test false #Fails to catch the non-integer dim
+    catch e
+        @test e == "SLFA.Gaussian: dim must be an integer."
+    end
     
-    rbf_5D_iso = Gaussian(x0, w)
-    @test rbf_5D_iso isa Gaussian{Isotropic, typeof(x0[1]), length(x0)}
-    @test rbf_5D_iso(x0) == 1.0
-    @test rbf_5D_iso([2.0,3.0,4.0,5.0,6.0]) == 1.300729765406762e-05
+    try
+        rbf = Gaussian{Anisotropic{Aligned}, typeof(x0[1]), 3}(x0, w)
+        @test false #Fails to catch the non-matching dim
+    catch e
+        @test e == "SLFA.Gaussian: dim does not match length(x0)."
+    end
+    try
+        x_empty = Float64[]
+        rbf = Gaussian{Anisotropic{Aligned}, typeof(x0[1]), 0}(x_empty, w)
+        @test false #Fails to catch the empty x0
+    catch e
+        @test e == "SLFA.Gaussian: x0 = []"
+    end
+    try
+        rbf = Gaussian{Anisotropic{Aligned}, typeof(x0[1]), 1}([x0[1]], w)
+        @test false #Fails to catch that anisotropic shouldnt be 1D
+    catch e
+        @test e == "SLFA.Gaussian: Anisotropic constructor should not be used for 1D Gaussians."
+    end
+    try
+        rbf = Gaussian{Anisotropic{Aligned}, typeof(x0[1]), 2}(x0, [w[1]])
+        @test false #Fails to catch that x0 and w dimensions dont match
+    catch e
+        @test e == "SLFA.Gaussian: length of x0 ($(length(x0))) does not match length of w ($(length([w[1]])))."
+    end
     
-    # Test with parametric constructor
-    rbf_iso_param = Gaussian{Isotropic,typeof(x0[1]),length(x0)}(x0,w)
-    @test rbf_iso_param isa Gaussian{Isotropic, typeof(x0[1]), length(x0)}
-    
-    # Test with an array of parameters
-    theta = [x0;w]
-    rbf_iso_paramarray = Gaussian{Isotropic,typeof(x0[1]),length(x0)}(theta)
-    @test rbf_iso_paramarray isa Gaussian{Isotropic, typeof(x0[1]), length(x0)} 
+    rbf = Gaussian{Anisotropic{Aligned}, typeof(x0[1]),length(x0)}(x0,w)
+    @test rbf.x0 == [1.0, 1.0]
+    @test rbf.w == [2.0,2.0]
 end
 
-#Tests 15-17
-@testset  "Evaluating a Vector/Vector 1D Gaussian RBF" begin
-    x0 = [1.0];
-    w = [2.0];
-    rbf_iso = Gaussian(x0, w) #10,11,12
-    @test rbf_iso isa Gaussian{Isotropic, typeof(x0[1]), 1} 
-    @test rbf_iso(x0[1]) == 1.0 
-    @test rbf_iso(2) == 0.01831563888873418 #Add a rounding handler here
-end
-
-#Tests 18-22
-@testset "Evaluating an ND Anisotropic Gaussian RBF" begin
-    x0 = [1.0, 2.0, 3.0, 4.0, 5.0];
-    w = [1.0, 1.5, 2.0, 2.5, 3.0];
-    
-    rbf_5D_aniso = Gaussian(x0, w)#13,14,15
-    @test rbf_5D_aniso isa Gaussian{Anisotropic{Aligned}, typeof(x0[1]), length(x0)}
-    @test rbf_5D_aniso(x0) == 1.0
-    @test rbf_5D_aniso([2.0,3.0,4.0,5.0,6.0]) == 1.6918979226151304e-10
-    
-    # Test with parametric constructor
-    rbf_5D_param_aniso = Gaussian{Anisotropic{Aligned},typeof(x0[1]),length(x0)}(x0,w)
-    @test rbf_5D_param_aniso isa Gaussian{Anisotropic{Aligned},typeof(x0[1]),length(x0)}
-    
-    # Test with an array of parameters
-    theta = [x0;w]
-    rbf_aniso_paramarray = Gaussian{Anisotropic{Aligned},typeof(x0[1]),length(x0)}(theta)
-    @test rbf_aniso_paramarray isa Gaussian{Anisotropic{Aligned}, typeof(x0[1]), length(x0)} 
+@testset "Gaussian(x0::T_x, w::T_x): 1D RBF Outer Constructor" begin
+    x0 = 1.0;
+    w = 2.0;
+    rbf = Gaussian(x0, w)
+    @test rbf isa Gaussian{Isotropic, typeof(x0[1]), 1} 
+    @test rbf.x0 == 1.0
+    @test rbf.w == 2.0
 end
